5a20f8275a4b69f50bd70a2d5100484584f41f4c
samasno <samasno@LAPTOP-HJT8OQVR.localdomain>
2021-01-19 19:43:23 +0400

    Seed script setup

---
+++ server/db/models/Review.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+exports.ReviewSchema = mongoose.Schema({
+  author: {
+    type: String,
+    require: true,
+    minlength: 3,
+    maxlength: 40
+  },
+  date: Date,
+  rating: {
+    type: Number,
+    max: 5,
+    min: 1,
+    required: true
+  },
+  content: {
+    type: String,
+    maxlength: 150,
+    minlength: 1
+  }
+});
+
+const reviewDataSchema = mongoose.Schema({
+  workspaceSlug: String,
+  workspaceId: Number,
+  total: {
+    type: Number,
+    default: 0
+  },
+  avg: Number,
+  reviewCount: {
+    type: Number,
+    default: 0
+  },
+  reviews: [exports.ReviewSchema]
+});
+
+reviewDataSchema.pre('save', function(next) {
+  this.total = this.reviews.reduce((total, { rating }) => total + rating, 0);
+  this.reviewCount = this.reviews.length;
+  this.avg = (this.total / this.reviewCount).toFixed(1);
+  next();
+})
+
+
+exports.ReviewData = mongoose.model('ReviewData', reviewDataSchema);
