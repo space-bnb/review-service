ad74b054ca98983f70cdc9c9debb47ee4648491c
samasno <samasno@LAPTOP-HJT8OQVR.localdomain>
2021-01-30 10:31:34 +0400

    Components connected to api, basic error handling

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default () => {
+// this component will render individual reviews
+export default ({ review }) => {
+  // convert ISO date-time to string and add to state
+  let rawDate = '';
+  if (review.date) {
+    rawDate = new Date (review.date).toString().split(' ').slice(1, 4);
+    rawDate[1] = rawDate[1] + ', ';
+    rawDate = rawDate.join(' ');
+  }
+
+  // check content needs to be truncated
+  const limitContent = review.content.length > 40 ? true : false;
+
+  //add to state
+  const [date, setDate] = useState(rawDate);
+  const [readLimit, setLimit] = useState(limitContent);
+
+  // function to remove readLimit to see all content
+  const handleReadMore = () => {
+    setLimit(false);
+  };
+
+  const ReadMore = () => <a className="blue-links" onClick={handleReadMore}>&nbsp; Read more</a>;
+
   return (
     <div className="reviews-section-card">
-      <p className="reviews-section-author">Real Person</p>
-      <p className="reviews-section-date">10 November 1775</p>
+      <p className="reviews-section-author">{ review.author }</p>
+      <p className="reviews-section-date">{ date || '' } </p>
       <div className="reviews-section-stars">
         <span className="fas fa-star fa-xs star-small"></span><span className="fas fa-star fa-xs star-small"></span><span className="fas fa-star fa-xs star-small"></span>
       </div>
       <div className="reviews-section-content">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, dolorum. Molestiae dolore repellat, laboriosam quia rerum labore perspiciatis architecto tempore expedita dolor iste quis eaque at earum dolorem minus temporibus eius ducimus deserunt inventore consequatur! 
-          <br/>
-          <a href="">Read more</a>
+        <p>
+          {/* if content is long, truncate and display read more button */}
+          { readLimit ? review.content.slice(0, 40).concat(' ...') : review.content }
+          { readLimit ?  <ReadMore /> : '' }
+        </p>
+
       </div>
     </div>
-  )
+  );
 };
--- client/src/components/Stats.jsx
+++ client/src/components/Stats.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { getReviewInfo, getWorkspaceId } from '../actions/index.js';
 
+// displays under title, shows average and number of reviews
+// renders 0 values on no record and message on error
+export default ({ reviewInfo = null }) => {
 
-export default () => (
-  <div className="reviews-section-stats">
-    <span className="fas fa-sm fa-star star"></span>
-    4.3 <span className="grey-line">|</span> 3 Google reviews
-  </div>
-);
+  const [info, setInfo] = useState(reviewInfo);
+  // get reviewInfo and simple error handling
+  if (info === null) {
+    getReviewInfo(getWorkspaceId())
+      .then(({ data }) => {
+        if (data.status === 404) {
+          setInfo({avg: 0, reviewCount: 0});
+        } else {
+          setInfo(data);
+        }
+      })
+      .catch(() => setInfo(false));
+  }
+
+  if (info === null) {
+    return <div></div>;
+  }
+
+  if (info === false) {
+    return <div><h3>There was an error, please try again later.</h3></div>;
+  }
+
+  const { avg, reviewCount } = info;
+  
+  return (
+    <div className="reviews-section-stats">
+      <span className="fas fa-sm fa-star star"></span>
+      { avg } <span className="grey-line">|</span> { reviewCount } Google reviews
+    </div>
+  );
+
+};
