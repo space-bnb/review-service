3740ff64e0486a2a9ea216fd5b6db8fe70fb9959
samasno <samasno@LAPTOP-HJT8OQVR.localdomain>
2021-02-10 07:17:53 +0400

    Moved upload out of setTimout

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,11 @@ const { upload } = require('./utils/s3upload');
 module.exports = function(grunt) {
 
   grunt.registerTask('s3', function() {
-      let done = this.async();
-      setTimeout(function () {
-        upload().then(() => done());
+    
+    let done = this.async();
+    upload();
+    setTimeout(function () {
+      done();
     }, 5000);
   });
 };
