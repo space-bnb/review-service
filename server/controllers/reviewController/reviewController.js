c8161508c470a65e6e6c816101f0c9135749b35d
Dane Murphy <danem7@gmail.com>
2021-03-06 15:33:02 -0800

    commit before deleting the PUT route that deletes a review

--- server/controllers/reviewController/reviewController.js
+++ server/controllers/reviewController/reviewController.js
@@ -17,10 +17,30 @@ router.get('/:workspaceId', async (req, res) => {
 router.post('/:workspaceId', async (req, res) => {
     const repo = new ReviewRepository(req.params.workspaceId);
     try {
-        const success = await repo.create(req.body);
-        return res.status(201).json(success);
+        const newReview = await repo.create(req.body);
+        return res.status(201).json(newReview);
     } catch (error) {
         console.log(error);
+        return res.status(500).json({ message: serverError });
+    }
+});
+
+router.put('/:workspaceId', async (req, res) => {
+    const repo = new ReviewRepository(req.params.workspaceId);
+    try {
+        const updatedReview = await repo.update(req.body);
+        return res.status(200).json(updatedReview);
+    } catch (error) {
+        return res.status(500).json({ message: serverError });
+    }
+});
+
+router.delete('/:workspaceId', async (req, res) => {
+    const repo = new ReviewRepository(req.params.workspaceId);
+    try {
+        await repo.delete(req.body.reviewId);
+        return res.status(204).end();
+    } catch (error) {
         return res.status(500).json({ message: serverError });
     }
 });
--- server/controllers/reviewController/reviewRepository.js
+++ server/controllers/reviewController/reviewRepository.js
@@ -1,8 +1,10 @@
+const ReviewDataRepository = require('../reviewDataConroller/reviewDataRepository');
 const { ReviewData } = require('../../db/models/Review');
 
 class ReviewRepository {
     constructor(workspaceId) {
         this.workspaceId = workspaceId != null ? workspaceId : null;
+        this.reviewDataRepository = new ReviewDataRepository(this.workspaceId);
     }
 
     async get() {
@@ -20,6 +22,23 @@ class ReviewRepository {
             const updatedReviewData = await reviewData.save();
             return updatedReviewData.reviews[updatedReviewData.reviews.length - 1];
         }
+    }
+
+    async update(updatedReview) {
+        const reviewData = await this.reviewDataRepository.getById();
+        const reviewIndex = reviewData.reviews.findIndex((review) => String(review._id) === String(updatedReview._id));
+
+        reviewData.reviews[reviewIndex] = updatedReview;
+        const updatedReviewData = await reviewData.save();
+        return updatedReviewData.reviews[reviewIndex];
+    }
+
+    async delete(reviewId) {
+        const reviewData = await this.reviewDataRepository.getById();
+        const updatedReviews = reviewData.reviews.filter((review) => String(reviewId) !== String(review._id));
+
+        reviewData.reviews = updatedReviews;
+        return reviewData.save();
     }
 }
 
