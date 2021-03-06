44b6b3e43c627009cdb75e6036444ffcc87b8ee9
Dane Murphy <danem7@gmail.com>
2021-03-05 17:13:10 -0800

    adjusted connection logic

--- server/db/index.js
+++ server/db/index.js
@@ -1,23 +1,35 @@
 const mongoose = require('mongoose');
 
-exports.connect = (URI, cb) => new Promise((resolve, reject) => {
-  console.log('URI', URI);
+// exports.connect = (URI, cb) => new Promise((resolve, reject) => {
+//   try {
+//     mongoose.connect(URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true
+//     }, (err, data) => {
+//       console.log('error', cb);
+//       if (cb) {
+//         resolve(cb(err, data));
+//       } else {
+//         console.log('ERROR ON LINE 12');
+//         if(err) reject(err);
+//         resolve(data);
+//       }
+//     });
+//   } catch (error) {
+//     reject('cannot connect to db');
+//   }
+// });
+
+exports.connect = async (URI) => {
   try {
-    mongoose.connect(URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    }, (err, data) => {
-      if (cb) {
-        resolve(cb(err, data));
-      } else {
-        console.log('ERROR ON LINE 12');
-        if(err) reject(err);
-        resolve(data);
-      }
-    });
+    const res = await mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true});
+    
+    if (!res) throw new Error();
+
+    console.log('Connected to Mongo Atlas');
   } catch (error) {
-    reject('cannot connect to db');
+    console.log('Could not connect to Mongo Atlas')
   }
-});
+}
 
 exports.close = () => mongoose.connection.close();
--- server/dev.js
+++ server/dev.js
@@ -1,9 +1,9 @@
 // config env vars
 const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../', '.env')});
+// require('dotenv').config({ path: path.resolve(__dirname, '../', '.env')});
 
 // connect db to dev data
 require('./db').connect(process.env.MONGO_URI_DEV);
 
 // // run server on port 5002
-require('./server');
+// require('./server');
