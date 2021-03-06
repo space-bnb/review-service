4f8cf79dcd4281d6cec00b685368785a783b66c0
Dane Murphy <danem7@gmail.com>
2021-03-05 17:36:50 -0800

    deleted old code

--- server/db/index.js
+++ server/db/index.js
@@ -1,25 +1,5 @@
 const mongoose = require('mongoose');
 
-// exports.connect = (URI, cb) => new Promise((resolve, reject) => {
-//   try {
-//     mongoose.connect(URI, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true
-//     }, (err, data) => {
-//       console.log('error', cb);
-//       if (cb) {
-//         resolve(cb(err, data));
-//       } else {
-//         console.log('ERROR ON LINE 12');
-//         if(err) reject(err);
-//         resolve(data);
-//       }
-//     });
-//   } catch (error) {
-//     reject('cannot connect to db');
-//   }
-// });
-
 exports.connect = async (URI) => {
   try {
     const res = await mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true});
