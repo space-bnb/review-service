4389c38e2c7fff1dab7b091338b19d5f5b14e5b7
Dane Murphy <danem7@gmail.com>
2021-03-06 16:41:13 -0800

    commit before deleting index

--- server/server.js
+++ server/server.js
@@ -1,7 +1,5 @@
 const path = require('path');
-require('dotenv').config({ path: path.join(__dirname, '../', '.env') });
 const express = require('express');
-const { notFound, errors } = require('./controllers/');
 const reviewInfoRoutes = require('./controllers/reviewDataConroller/reviewDataController');
 const reviewRoutes = require('./controllers/reviewController/reviewController');
 
@@ -15,7 +13,5 @@ app.use('/api/reviews/info', reviewInfoRoutes);
 app.use('/api/reviews/all', reviewRoutes);
 app.use('/', express.static(path.join(__dirname, '../', 'client', 'dist')));
 app.use('/buildings/:workspaceId', express.static(path.join(__dirname, '../', 'client', 'dist')));
-app.get('*', notFound);
-app.use(errors);
 
 module.exports = app;
