92f1109c3af00cdb95be6e4bd2565aa817a06d17
Dane Murphy <danem7@gmail.com>
2021-03-08 01:57:10 -0800

    changed iterations to 19. testing out automated data generation setup

--- server/dataGeneration/generateDump.js
+++ server/dataGeneration/generateDump.js
@@ -3,7 +3,7 @@ const path = require('path');
 const faker = require('faker');
 const command = require('./command');
 
-const iterations = 1;
+const iterations = 19;
 
 for (let k = 0; k <= iterations; k++) {
     let query = '';
